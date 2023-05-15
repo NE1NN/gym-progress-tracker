dd4c4a2f257bd50123e44bb1e4b5f3ce7a2ce110
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-05-15 17:09:31 +1000

    Add firebase to store data

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA565I3Ks5HUTXJXf0iur_X_T4sKiueuM4",
+  authDomain: "gym-tracker-a2461.firebaseapp.com",
+  projectId: "gym-tracker-a2461",
+  storageBucket: "gym-tracker-a2461.appspot.com",
+  messagingSenderId: "460151162262",
+  appId: "1:460151162262:web:f7b5dc0511ddf0f8a549b7",
+  measurementId: "G-MC3DMBRGPV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
